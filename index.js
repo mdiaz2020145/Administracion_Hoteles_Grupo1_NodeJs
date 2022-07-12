667f27be158cddff964360af96eafddd902c8d5d
Marcelo Diaz <88795578+mdiaz2020145@users.noreply.github.com>
2022-07-12 00:29:33 -0600

    MongoDb atlas

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose')
 const app = require('./app')
+require('dotenv').config()
+
 UsuarioController = require('./src/controllers/usuario.controller');
 
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost:27017/HotelesGrupo1', {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+mongoose.connect(process.env.HOTELES_DB_CONNECTION,{useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
     console.log('Conexion a la base de datos exitosa :D')
 
-    app.listen(3000, function () {
-        console.log('Corriendo en el puerto 3000 :D') 
+    app.listen(process.env.PORT || 3000, function () {
+        console.log('Corriendo en el puerto 3000 :D')
     })
 
     //Registrar super Admin 
